a42558063552a0645b1ebe3f8c2b73ae9e6bb13a
jzyt2022 <161529149+jzyt2022@users.noreply.github.com>
2024-04-10 19:53:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 systemctl stop firewalld.service
 systemctl disable firewalld.service
 #BBR
-wget –no-check-certificate https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/install_bbr.sh 
+wget –no-check-certificate raw.githubusercontent.com/jzyt2022/v2/main/install_bbr.sh 
 chmod +x install_bbr.sh 
 ./install_bbr.sh
 
 #v2fly
-wget https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/v2fly_install.sh
+wget raw.githubusercontent.com/jzyt2022/v2/main/v2fly_install.sh
 chmod -R 777 v2fly_install.sh
 ./v2fly_install.sh
 
-#wget -cO /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/v2fly_config.json
+#wget -cO /usr/local/etc/v2ray/config.json raw.githubusercontent.com/jzyt2022/v2/main/v2fly_config.json
 chmod -R 777 /usr/local/etc/v2ray/config.json
 echo '{"inbounds":[{"port":443,"protocol":"vmess","settings":{"clients":[{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","alterId":0}]}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > /usr/local/etc/v2ray/config.json 
 #gost
